badf4ae19f3b266dea7e4ea24e98faa7e0891c28
Nick Roberts <djparadyme@gmail.com>
2019-02-10 18:53:15 -0500

    :sparkles: Build script

--- config/webpack/webpack.gh-pages.js
+++ config/webpack/webpack.gh-pages.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge');
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.prod');
 
 module.exports = merge.smart(common, {
